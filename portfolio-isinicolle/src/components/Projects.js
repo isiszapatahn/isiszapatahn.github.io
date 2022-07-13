df2d35d74cd7b0b0c44b99515948e04479c361c8
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-13 16:55:13 -0600

    .
    .

--- portfolio-isinicolle/src/components/Projects.js
+++ portfolio-isinicolle/src/components/Projects.js
@@ -2,9 +2,11 @@ import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 
 /*projects image */
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import projImg1 from "../assets/img/Proyectos/proyectos-signalclone.png";
+import projImg2 from "../assets/img/Proyectos/proyectos-signalclone.png";
+import projImg3 from "../assets/img/Proyectos/proyectos-signalclone.png";
+
+import proj1 from "../assets/img/Proyectos/proyectos-signalclone.png";
 
 
 import colorSharp2 from "../assets/img/color-sharp2.png";
