21ba091d672784e93bea8e669ac6f510e6bd03af
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-13 13:49:01 -0600

    .
    .

--- portfolio-isinicolle/src/components/Skills.js
+++ portfolio-isinicolle/src/components/Skills.js
@@ -35,23 +35,39 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <p>I really like the area of computing and I feel that I am a very good professional.<br></br> Being an IT professional has led me to have different skills. To mention some of them are:</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                            <div className="item">
+                                <img src={meter2} alt="Image" />
+                                <h5>Desktop Development</h5>
+                            </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
                                 <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>Mobile Development</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>UI/UX Design</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="Image" />
+                                <h5>Cloud Developer</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image" />
+                                <h5>Design Graphic</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image" />
+                                <h5>Frontend</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image" />
+                                <h5>Backend</h5>
                             </div>
                         </Carousel>
                     </div>
